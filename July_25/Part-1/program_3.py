7c86c4d9f151b7be38ceece0d7b412736ba0a849
ffdrko <fahim.deepto@northsouth.edu>
2025-07-03 23:09:47 +0600

    Type casting

---
+++ July_25/Part-1/program_3.py
@@ -0,0 +1,22 @@
+a = 34
+b = "34"
+d = 223
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+c = int(b)
+
+print(c)
+print(type(c))
+
+print(d)
+print(type(d))
+
+e  = str(d)
+
+print(e)
+print(type(e))
