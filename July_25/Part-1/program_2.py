09b95ae2380826dc7bcb080547cb8c8c89b5bd94
ffdrko <fahim.deepto@northsouth.edu>
2025-07-03 23:02:34 +0600

    Variable and Data types

---
+++ July_25/Part-1/program_2.py
@@ -0,0 +1,13 @@
+age = 34
+name = "Deepto"
+cgpa = 4.55
+is_completed = True
+
+print(age)
+print(type(age))
+print(name)
+print(type(name))
+print(cgpa)
+print(type(cgpa))
+print(is_completed)
+print(type(is_completed))
