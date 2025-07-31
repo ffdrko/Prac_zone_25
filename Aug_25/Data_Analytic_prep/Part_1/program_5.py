c266523138a6994e242a1690fbbebb7fed2a8e00
ffdrko <fahim.deepto@northsouth.edu>
2025-07-31 18:48:56 +0600

    problem solving in python

---
+++ Aug_25/Data_Analytic_prep/Part_1/program_5.py
@@ -0,0 +1,46 @@
+# Problem solving - 1
+
+name = "Fahim"
+age = 20
+address = "Mirpur Dohs"
+
+print(name)
+print(age)
+print(address)
+
+# Problem solving - 2
+
+a = 12
+b = 13
+
+print(a, b)
+
+a, b = b, a
+
+print(a, b)
+
+# problem solving - 3
+
+x = 3.25
+
+print(x, type(x))
+
+y = int(x)
+
+print(y, type(y))
+
+# problem solving - 4
+
+print("Student details")
+
+name = input("Enter the student name : ")
+stud_id = int(input("Enter the student id : "))
+
+print("The student name is", name)
+print("The student id is ", stud_id)
+
+# problem solving - 5
+
+num = int(input("Enter the number : "))
+
+print(float(num), type(num))
