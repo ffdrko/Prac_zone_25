662b89932cb1f73d32efc3bbe9e2ccfe64a398b6
ffdrko <fahim.deepto@northsouth.edu>
2025-07-31 18:00:18 +0600

    Using print statement for multiple line.

--- Aug_25/Data_Analytic_prep/Part_1/program_0.py
+++ Aug_25/Data_Analytic_prep/Part_1/program_0.py
@@ -1,3 +1,10 @@
 print("Hello World")
 print("This is my first program")
 print("This is next line.")
+
+
+print("""Hello World,
+This is my first program,
+I hope you all like it.""")
+
+print("This is my second program\nI hope you all like it.")
