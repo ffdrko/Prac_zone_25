a2da9d4a855fdbfd763b3ac7d1bca4f495f26a37
ffdrko <fahim.deepto@northsouth.edu>
2025-07-31 18:28:45 +0600

    Type casting in python

---
+++ Aug_25/Data_Analytic_prep/Part_1/program_4.py
@@ -0,0 +1,14 @@
+# Typecasting - changing one datatype into another datatype.
+
+num1 = 132
+num2 = 1.23
+
+print(type(num1))
+print(type(num2))
+
+num3 = num1 + num2
+
+print(type(num3))
+
+num3 = int(num3)
+print(type(num3))
