2f7422596d5fc58b56ea2c9625ff8ac9cb84492e
ffdrko <fahim.deepto@northsouth.edu>
2025-07-31 18:23:39 +0600

    Datatype and user input in python

---
+++ Aug_25/Data_Analytic_prep/Part_1/program_3.py
@@ -0,0 +1,10 @@
+# Datatypes & user input
+
+name = input("Enter your name: ")
+print("The name is " + name)
+
+age = input("Enter your age: ")
+print("The age is " + age)
+
+exp1 = eval(input("Enter any equation here : "))
+print(exp1)
