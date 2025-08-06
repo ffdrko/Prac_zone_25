f149a96f754aa18719039b6f3bdb9ab473333190
ffdrko <fahim.deepto@northsouth.edu>
2025-08-06 12:38:20 +0600

    SQL Basic query done

---
+++ Aug_25/Data_Analytic_prep/SQL/Part_1/SQL_14.sql
@@ -0,0 +1,5 @@
+-- Retrieve Top 3 Customers with the highest scroes
+
+SELECT TOP 3 *
+FROM customers
+ORDER BY score DESC
---
+++ Aug_25/Data_Analytic_prep/SQL/Part_1/SQL_15.sql
@@ -0,0 +1,5 @@
+-- Retrieve the lowest 2 customers based on the score
+
+SELECT TOP 2 *
+FROM customers
+ORDER BY score ASC
