cff8e02b9ec296b5e1aa279df784f5b56de7c62e
ffdrko <fahim.deepto@northsouth.edu>
2025-08-27 20:33:52 +0600

    Get all customers along with their orders, including those without orders

--- Sep_25/SQL/Part-5/SQLQuery_8.sql
+++ Sep_25/SQL/Part-5/SQLQuery_8.sql
@@ -1,6 +1,10 @@
 /*Get all customers along with their orders,
 but only for customers who have placed an order */
-SELECT *
-FROM customers
-INNER JOIN orders
-ON id = customer_id
+SELECT 
+	id,
+	first_name,
+	order_id,
+	sales
+FROM customers AS c
+INNER JOIN orders AS o
+ON c.id = o.customer_id
