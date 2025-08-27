a167adacf8b06a4e9607e547517c7aeae3d45568
ffdrko <fahim.deepto@northsouth.edu>
2025-08-27 20:48:14 +0600

    Get all customers along with their orders, including those without matching orders

---
+++ Sep_25/SQL/Part-6/SQLQuery_0.sql
@@ -0,0 +1,10 @@
+/*Get all customers along with their orders,
+including those without matching customers*/
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM customers AS c
+RIGHT JOIN orders AS o
+ON c.id = o.customer_id
