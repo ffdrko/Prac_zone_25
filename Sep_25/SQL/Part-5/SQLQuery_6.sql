8858ac29ae18b9d676d03dab72baee6da58854c2
ffdrko <fahim.deepto@northsouth.edu>
2025-08-27 20:03:54 +0600

    Retrieve all data from customers and orders on two different results

--- Sep_25/SQL/Part-5/SQLQuery_6.sql
+++ Sep_25/SQL/Part-5/SQLQuery_6.sql
@@ -1,4 +1,4 @@
 -- Find all customers whose first name has 'r' in the third position
 SELECT *
 FROM customers
-WHERE first_name like '__r%'
+WHERE first_name like '__r%' 
---
+++ Sep_25/SQL/Part-5/SQLQuery_7.sql
@@ -0,0 +1,7 @@
+/* Retrieve all data from customers and
+orders on two different results */
+SELECT *
+FROM customers;
+
+SELECT *
+FROM orders;
