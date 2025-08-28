37517e4476b43c1d7ef7b863d2730cf637bf5ec2
ffdrko <fahim.deepto@northsouth.edu>
2025-08-28 13:20:06 +0600

    Orders data are stored in separte tables(orders and ordersArchive) Combine all orders data into one report without duplicates.

---
+++ Sep_25/SQL/Part-7/SQLQuery_3.sql
@@ -0,0 +1,34 @@
+/* Orders data are stored in separte tables(orders and ordersArchive)
+Combine all orders data into one report without duplicates.*/
+SELECT 
+'Orders' AS SourceTable,
+       [OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.Orders
+UNION 
+SELECT 
+'OrdersArchie' AS SourceTable,
+[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.OrdersArchive
+ORDER BY OrderID
