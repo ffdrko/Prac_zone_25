8895e49ea6982554cbd07a321d0b7ab99d6f55de
ffdrko <fahim.deepto@northsouth.edu>
2025-08-31 13:05:01 +0600

    How many orders were placed each year.

--- Sep_25/SQL/Part-9/SQLQuery_2.sql
+++ Sep_25/SQL/Part-9/SQLQuery_2.sql
@@ -4,3 +4,10 @@ YEAR(OrderDate) Year_time,
 COUNT(*) Num_of_orders
 FROM Sales.Orders
 GROUP BY YEAR(OrderDate)
+
+-- How many orders were placed each month
+SELECT 
+DATENAME(MONTH, OrderDate) Year_time,
+COUNT(*) Num_of_orders
+FROM Sales.Orders
+GROUP BY DATENAME(MONTH, OrderDate)
