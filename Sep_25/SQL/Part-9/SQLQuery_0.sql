3b0406f694b8701571f0fbc1cc8966239b501e48
ffdrko <fahim.deepto@northsouth.edu>
2025-08-31 12:40:03 +0600

    DateTrunc function done.

--- Sep_25/SQL/Part-9/SQLQuery_0.sql
+++ Sep_25/SQL/Part-9/SQLQuery_0.sql
@@ -2,3 +2,9 @@ SELECT OrderID, CreationTime,
 DATENAME(WEEKDAY, CreationTime) Day_dn,
 DATENAME(month, CreationTime) Month_dn
 FROM Sales.Orders
+
+SELECT OrderID, CreationTime,
+DATETRUNC(YEAR, CreationTime) Year_dt,
+DATETRUNC(DAY, CreationTime) Day_dt,
+DATETRUNC(MINUTE, CreationTime) Min_dt
+FROM Sales.Orders
