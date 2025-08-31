2a636caaa261bcc806f2fc81e677fdd03a8cb256
ffdrko <fahim.deepto@northsouth.edu>
2025-08-31 18:49:59 +0600

    Calcualte the age of the employess

--- Sep_25/SQL/Part-9/SQLQuery_9.sql
+++ Sep_25/SQL/Part-9/SQLQuery_9.sql
@@ -4,3 +4,8 @@ OrderDate,
 DATEADD(MONTH, 3, OrderDate),
 DATEADD(year, 2, OrderDate)
 from Sales.Orders
+
+-- Calcualte the age of the employess
+SELECT FirstName,
+DATEDIFF(year, BirthDate, GETDATE()) AS AGE
+FROM Sales.Employees
